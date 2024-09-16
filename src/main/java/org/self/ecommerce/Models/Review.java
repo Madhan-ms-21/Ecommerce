fc53b621aec083ca001c73a721b009e9cb503b30
madkumar <iam.madhan2002@gmail.com>
2024-09-16 08:57:22 +0530

    intitialized the project with git repository

---
+++ src/main/java/org/self/ecommerce/Models/Review.java
@@ -0,0 +1,30 @@
+package org.self.ecommerce.Models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Review {
+    @Id
+    private Long id;
+
+    private String review;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @JsonIgnore
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime createdAt;
+}
